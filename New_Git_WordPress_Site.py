f3e616b5ee0295ff9afb8d1a736ba9880503003e
Josh Leach <45842827+CryptoidCoder@users.noreply.github.com>
2023-01-01 20:29:09 +0000

    Initial Commit

---
+++ New_Git_WordPress_Site.py
@@ -0,0 +1,11 @@
+import os
+
+'''cd "D:\Coding\Projects\Websites\Development" #Move To Development Websites Folder
+git clone git@github.com:WordPress/WordPress.git #Git Clone WordPress Master
+git remote rm master #Remove Remote
+git remote add origin '''
+
+# Move to dev websites folder
+# git clone wordpress
+# create new github repo @ github sites
+
